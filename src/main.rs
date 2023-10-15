bab258567008efcc741af4d3295856bde45fb47c
Julien Blanchard <jblanchard@makemusic.com>
2023-10-15 17:14:13 +0200

    Refresh

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // #![deny(warnings)]
-extern crate hyper;
-extern crate hyper_tls;
+extern crate reqwest;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
@@ -15,27 +14,13 @@ use regex::Regex;
 extern crate deunicode;
 use deunicode::deunicode;
 
-use hyper::Client;
-use hyper::rt::{self, Future, Stream};
-use hyper_tls::HttpsConnector;
-
 const API_URL: &'static str = "https://lobste.rs/hottest.json";
 
 fn main() {
-    let url = API_URL.parse().unwrap();
-
-    let fut = fetch_stories(url)
-        .map(|stories| {
-            create_finger_response(stories).unwrap();
-        })
-        .map_err(|e| {
-            match e {
-                FetchError::Http(e) => eprintln!("http error: {}", e),
-                FetchError::Json(e) => eprintln!("json parsing error: {}", e),
-            }
-        });
-
-    rt::run(fut);
+    let stories = match fetch_stories(API_URL.to_string()) {
+        Ok(stories) => create_finger_response(stories).unwrap(),
+        Err(e) => panic!(e),
+    };
 }
 
 fn create_finger_response(stories: Vec<Story>) -> std::io::Result<()> {
@@ -53,11 +38,24 @@ fn stories_to_finger(stories: Vec<Story>) -> String {
         } else {
             let re = Regex::new(r"^https").unwrap();
             let story_url = re.replace_all(&story.url, "http");
-            format!("\n[{}] - {}\n{}\n", story.score, deunicode(&story.title), story_url)
+            format!(
+                "\n[{}] - {}\n{}\n",
+                story.score,
+                deunicode(&story.title),
+                story_url
+            )
         };
 
-        let meta_line = format!("Submitted {} by {} | {}\n", pretty_date(&story.created_at), story.submitter_user.username, story.tags.join(", "));
-        let comments_line = format!("View {} comments {}\n", story.comment_count, story.short_id_url);
+        let meta_line = format!(
+            "Submitted {} by {} | {}\n",
+            pretty_date(&story.created_at),
+            story.submitter_user.username,
+            story.tags.join(", ")
+        );
+        let comments_line = format!(
+            "View {} comments {}\n",
+            story.comment_count, story.short_id_url
+        );
 
         finger.push_str(&story_line);
         finger.push_str(&meta_line);
@@ -70,28 +68,17 @@ fn pretty_date(date_string: &String) -> String {
     let parsed_date = date_string.parse::<DateTime<Utc>>();
     let date = match parsed_date {
         Ok(date) => date,
-        Err(_e)  => Utc::now(),
+        Err(_e) => Utc::now(),
     };
     date.format("%a %b %e %T %Y").to_string()
 }
 
-fn fetch_stories(url: hyper::Uri) -> impl Future<Item=Vec<Story>, Error=FetchError> {
-    let https = HttpsConnector::new(4).expect("TLS initialization failed");
-    let client = Client::builder()
-        .build::<_, hyper::Body>(https);
+fn fetch_stories(url: String) -> Result<Vec<Story>, reqwest::Error> {
+    let client = reqwest::blocking::Client::new();
+    let mut response = client.get(&url).send()?;
 
-    client
-        .get(url)
-        .and_then(|res| {
-            res.into_body().concat2()
-        })
-        .from_err::<FetchError>()
-        .and_then(|body| {
-            let stories = serde_json::from_slice(&body)?;
-
-            Ok(stories)
-        })
-        .from_err()
+    let stories: Vec<Story> = response.json()?;
+    Ok(stories)
 }
 
 #[derive(Deserialize, Debug)]
@@ -110,21 +97,4 @@ struct Story {
 #[derive(Deserialize, Debug)]
 struct User {
     username: String,
-}
-
-enum FetchError {
-    Http(hyper::Error),
-    Json(serde_json::Error),
-}
-
-impl From<hyper::Error> for FetchError {
-    fn from(err: hyper::Error) -> FetchError {
-        FetchError::Http(err)
-    }
-}
-
-impl From<serde_json::Error> for FetchError {
-    fn from(err: serde_json::Error) -> FetchError {
-        FetchError::Json(err)
-    }
 }
